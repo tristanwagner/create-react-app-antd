5f6a2442fe371c308a151cd60a81475366397117
Tristan WAGNER <t.wagner66250@gmail.com>
2020-09-06 00:18:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# create-react-app-antd
+
+A CLI to automatically clone the [create-react-app-antd](https://github.com/ant-design/create-react-app-antd) template.
+
+Greatly inspired by [create-express-api](https://github.com/w3cj/create-express-api).
+
+## Installation
+
+Install the CLI globally OR use npx:
+
+```sh
+npm install -g create-react-app-antd
+```
+
+## Usage
+
+```sh
+# with global install
+create-react-app-antd name-of-app
+# with npx
+npx create-react-app-antd name-of-app
+```
+
+This will create a directory with the given name, clone the [create-react-app-antd](https://github.com/antdesign/create-react-app-antd) template repo into it, and install dependencies.
---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+const { spawn } = require('child_process');
+
+const name = process.argv[2];
+if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
+  return console.log(`
+  Invalid directory name.
+  Usage: create-react-app-antd name-of-app  
+`);
+}
+
+const repoURL = 'https://github.com/ant-design/create-react-app-antd.git';
+
+runCommand('git', ['clone', repoURL, name])
+  .then(() => {
+    return runCommand('rm', ['-rf', `${name}/.git`]);
+  }).then(() => {
+    console.log('Installing dependencies...');
+    return runCommand('npm', ['install'], {
+      cwd: process.cwd() + '/' + name
+    });
+  }).then(() => {
+    console.log('Done! 🏁');
+    console.log('');
+    console.log('To get started:');
+    console.log('cd', name);
+    console.log('npm start');
+  });
+
+function runCommand(command, args, options = undefined) {
+  const spawned = spawn(command, args, options);
+
+  return new Promise((resolve) => {
+    spawned.stdout.pipe(process.stdout)
+    spawned.stderr.pipe(process.stderr)
+    spawned.on('close', () => {
+      resolve();
+    });
+  });
+}
