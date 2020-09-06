ec68f0332d3a4018106b1aca55204bf2ce9d48ae
Tristan WAGNER <t.wagner66250@gmail.com>
2020-09-07 00:09:35 +0200

    add yarn and help options

--- index.js
+++ index.js
@@ -3,15 +3,37 @@
 const { spawn } = require('child_process');
 const fs = require('fs');
 
-const name = process.argv[2];
-if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
-  return console.log(`
-  Invalid directory name.
-  Usage: create-react-app-antd name-of-app  
-`);
+const args = process.argv.slice(2);
+
+const helpMessage = `
+  Usage: create-react-app-antd <options> <app-name>
+
+  Options
+  -y|--y|-yank|--yank     Use yarn instead of npm.
+  -h|-help|--h|--help     Display this message.`;
+
+if (args.find(arg => arg.match(/^(-|--)(h|help)$/))) {
+  return console.log(helpMessage);
 }
 
-const platform = /^win/.test(process.platform) ? 'win' : 'unix'
+const yarnIndex = args.findIndex(arg => arg.match(/^(-|--)(y|yarn)$/));
+let yarnFlag = false;
+
+if (yarnIndex >= 0) {
+  args.splice(yarnFlag, 1);
+  yarnFlag = true;
+}
+
+const name = args[0];
+
+if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/) || fs.existsSync(name)) {
+  return console.log(`
+  Invalid or already existing directory name.
+  ${helpMessage}
+  `);
+}
+
+const platform = /^win/.test(process.platform) ? 'win' : 'unix';
 
 const repoURL = 'https://github.com/ant-design/create-react-app-antd.git';
 
@@ -20,7 +42,8 @@ const gitDir = `${name}${platform === 'win'? '\\' : '/'}.git`;
 const commands = {
   git: ['git', ['clone', repoURL, name]],
   rm: platform === 'win' ? ['cmd', ['/c', 'rmdir', '/s', '/q', gitDir]] : ['rm', ['-rf', gitDir]],
-  npm: [platform === 'win' ? 'npm.cmd' : 'npm', ['install']]
+  npm: [platform === 'win' ? 'npm.cmd' : 'npm', ['install']],
+  yarn: [platform === 'win' ? 'yarn.cmd' : 'yarn', ['install']]
 };
 
 runCommand(...commands.git)
@@ -32,7 +55,7 @@ runCommand(...commands.git)
     }
   }).then(() => {
     console.log('Installing dependencies...');
-    return runCommand(...commands.npm, {
+    return runCommand(...commands[yarnFlag ? 'yarn' : 'npm'], {
       cwd: process.cwd() + '/' + name
     });
   }).then(() => {
@@ -40,15 +63,15 @@ runCommand(...commands.git)
     console.log('');
     console.log('To get started:');
     console.log('cd', name);
-    console.log('npm start');
+    console.log(yarnFlag ? 'yarn start' : 'npm start');
   });
 
 function runCommand(command, args, options = undefined) {
   const spawned = spawn(command, args, options);
 
   return new Promise((resolve) => {
-    spawned.stdout.pipe(process.stdout)
-    spawned.stderr.pipe(process.stderr)
+    spawned.stdout.pipe(process.stdout);
+    spawned.stderr.pipe(process.stderr);
     spawned.on('close', () => {
       resolve();
     });
